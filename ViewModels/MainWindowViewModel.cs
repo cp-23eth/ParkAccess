aa5639a94c43ea96d06d15878b8be2e13b1f8c99
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-04-02 07:59:43 +0200

    ajout de l'allumage + extinction du Shelly via un toggleButton

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,89 @@
-﻿namespace ParkAccess.ViewModels
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Avalonia.Controls;
+using System.Text.Json;
+using Microsoft.Graph.Models;
+
+namespace ParkAccess.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        private static readonly HttpClient client = new HttpClient();
+        string ip = "";
+        public bool status { get; set; }
+
+        
+
+        private async Task SendShellyCommand(string ip,string state)
+        {
+            try
+            {
+                string url = $"http://{ip}/relay/0?turn={state}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Commande {state} envoyée avec succès !");
+                }
+                else
+                {
+                    Console.WriteLine($"Erreur : {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception : {ex.Message}");
+            }
+        }
+
+        private async Task chooseCommand(string ip)
+        {
+            try
+            {
+                string url = $"http://{ip}/relay/0";
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    using JsonDocument doc = JsonDocument.Parse(jsonResponse);
+                    bool isOn = doc.RootElement.GetProperty("ison").GetBoolean();
+
+                    if (isOn == true)
+                    {
+                        status = true;
+                    } 
+                    else
+                    {
+                        status = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Erreur de connexion");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception : {ex.Message}");
+            }
+        }
+
+        [RelayCommand]
+        public async Task ActionOne()
+        {
+            ip = "157.26.121.53";
+
+            await chooseCommand(ip);
+
+            if (status == true)
+            {
+                await SendShellyCommand(ip, "off");
+            }
+            else
+            {
+                await SendShellyCommand(ip, "on");
+            }
+        }
     }
 }
