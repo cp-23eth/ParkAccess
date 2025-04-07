505a430feb71d3d98b6f41987babeb95426e14cf
Simon-Golay <simon.golay@ceff.ch>
2025-04-07 13:32:43 +0200

    Show Parking in grid

--- Parking.cs
+++ Parking.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ParkAccess
 {
     public class Parking
     {
+        [JsonPropertyName("nom")]
         public string Nom { get; set; }
+
+        [JsonPropertyName("mail")]
         public string Mail { get; set; }
+
+        [JsonPropertyName("ceff")]
         public string Ceff { get; set; }
+
+        [JsonPropertyName("ip")]
         public string Ip { get; set; }
+
+        public Parking() { }
+
         public Parking(string nom, string mail, string ceff, string ip)
         {
             Nom = nom;
             Mail = mail;
             Ceff = ceff;
             Ip = ip;
+        }
+
+        public override string ToString()
+        {
+            return $"Nom: {Nom}, Mail: {Mail}, Ceff: {Ceff}, Ip: {Ip}";
         }
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,9 @@ using System.Text.Json;
 using System.Collections.ObjectModel;
 using Microsoft.Graph.Models;
 using System.Collections.Generic;
+using static Microsoft.Graph.Constants;
+using Serilog;
+using Avalonia.Threading;
 
 namespace ParkAccess.ViewModels
 {
@@ -13,19 +16,57 @@ namespace ParkAccess.ViewModels
     {
         private static readonly HttpClient client = new HttpClient();
         string ip = "";
+        string url = "https://localhost:7159/api/calendar/parkings";
         public bool status { get; set; }
 
-        public ObservableCollection<Parking> Parkings { get;}
+        public ObservableCollection<Parking> Parkings { get; } = new ObservableCollection<Parking>();
 
         public MainWindowViewModel ()
         {
-            var parking = new List<Parking>
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("log.txt")
+                .CreateLogger();
+
+            InitializeParkings();
+        }
+
+        public async void InitializeParkings()
+        {
+            try
             {
-                new Parking("Pierre-Jolissaint", "Pierre-Jolissaint.parking@iceff.ch", "Industrie", "157.26.121.184"),
-                new Parking("Pierre-Jolissaint", "Pierre-Jolissaint.parking@iceff.ch", "Industrie", "157.26.121.184"),
-                new Parking("Pierre-Jolissaint", "Pierre-Jolissaint.parking@iceff.ch", "Industrie", "157.26.121.184")
-            };
-            Parkings = new ObservableCollection<Parking>(parking);
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Log.Information(responseBody);
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var parkings = JsonSerializer.Deserialize<ObservableCollection<Parking>>(responseBody, options);
+
+                Log.Information($"Count parkings: {parkings?.Count}");
+                if (parkings != null)
+                {
+                    Log.Information($"Nombre de parkings désérialisés : {parkings.Count}");
+                    await Dispatcher.UIThread.InvokeAsync(() =>
+                    {
+                        Parkings.Clear();
+                        foreach (var parking in parkings)
+                        {
+                            Log.Information($"Ajout du parking : Nom: {parking.Nom}, Mail: {parking.Mail}, Ceff: {parking.Ceff}, Ip: {parking.Ip}");
+                            Parkings.Add(parking);
+                        }
+                    });
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                
+            }
+            
         }
 
         private async Task SendShellyCommand(string ip,string state)
